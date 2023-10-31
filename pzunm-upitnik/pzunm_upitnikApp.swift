7f8d06651a186e51caab88b564a50c2e9075729c
Немања Аврамовић <nemanja.kojekude@gmail.com>
2023-10-31 11:43:42 +0100

    initial commit with just the project

---
+++ pzunm-upitnik/pzunm_upitnikApp.swift
@@ -0,0 +1,17 @@
+//
+//  pzunm_upitnikApp.swift
+//  pzunm-upitnik
+//
+//  Created by Немања Аврамовић on 31.10.23..
+//
+
+import SwiftUI
+
+@main
+struct pzunm_upitnikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
